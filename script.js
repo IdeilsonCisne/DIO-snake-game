e26695b81976a504baf6bc2a7539e6333391dcc6
Ideilson Cisne <ideilson@lit.ifce.edu.br>
2021-06-08 12:23:44 -0300

    Added food for snake

--- script.js
+++ script.js
@@ -10,6 +10,10 @@ snake[0] = {
 }
 
 let direction = "right";
+let food = {
+  x: Math.floor(Math.random() * 15 + 1) * box,
+  y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 function criarBG() {
   context.fillStyle = "lightgreen";
@@ -21,6 +25,11 @@ function criarCobrinha() {
     context.fillStyle = "green"; //Define cor verde
     context.fillRect(snake[i].x, snake[i].y, box, box); //Define tamanho
   }
+}
+
+function drawFood() {
+  context.fillStyle = "red";
+  context.fillRect(food.x, food.y, box, box)
 }
 
 document.addEventListener('keydown', update);
@@ -40,6 +49,7 @@ function iniciarJogo() {
 
   criarBG();
   criarCobrinha();
+  drawFood();
 
   let snakeX = snake[0].x;
   let snakeY = snake[0].y;
